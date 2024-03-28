2161572a555a2f1b0f715b3513d053ba6223f261
Yannik Marchand <ymarchand@me.com>
2024-03-28 10:24:40 +0100

    Add abnamro.managers to setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ long_description = \
 
 setuptools.setup(
 	name = "abnamro",
-	version = "2.0.0",
+	version = "2.0.1",
 	description = "Unofficial ABN AMRO client",
 	long_description = long_description,
 	author = "Yannik Marchand",
 	author_email = "ymarchand@me.com",
 	url = "https://github.com/kinnay/ABN-Amro",
-	packages = ["abnamro"],
+	packages = ["abnamro", "abnamro.managers"],
 	install_requires = [
 		"httpx ~= 0.25",
 		"pycryptodome"
